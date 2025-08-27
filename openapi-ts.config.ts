ce7b7109f930ead256af36f7eeb38e360ea27070
zdeblor <radek.zdeblo@ventrata.com>
2025-08-27 17:02:14 +0200

    chore: package json

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -1,22 +1,22 @@
 // openapi-ts.config.ts
 import { defineConfig } from '@hey-api/openapi-ts';
-import path from 'path'
+import path from 'node:path';
 
 export default defineConfig({
-    input: path.resolve(process.cwd(), 'src/openapi.yaml'), // or the correct path you found
-    output: {
-        format: 'prettier',
-        lint: 'eslint',
-        path: './src/models',
-    },
-    plugins: [
-        {
-            name: '@hey-api/typescript',
-            enums: 'typescript',
-        },
-        {
-            name: 'zod',
-            compatibilityVersion: 3
-        },
-    ],
+	input: path.resolve(process.cwd(), 'src/openapi.yaml'), // or the correct path you found
+	output: {
+		format: 'prettier',
+		lint: 'eslint',
+		path: './src/models',
+	},
+	plugins: [
+		{
+			name: '@hey-api/typescript',
+			enums: 'typescript',
+		},
+		{
+			name: 'zod',
+			compatibilityVersion: 3,
+		},
+	],
 });
