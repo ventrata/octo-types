c2613864d3223bb9ee4602f10522f5c1ea6cd104
Dominik Chrastek <dominik.chrastek@gmail.com>
2025-09-01 12:18:37 +0200

    add delete webhook

---
+++ src/models/DeleteWebhookRequestHeaders_octoCapabilities.ts
@@ -0,0 +1,8 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * A list of the Capabilities (their IDs) initialized with your request. ventrata/getyourguide capability is required
+ */
+export type DeleteWebhookRequestHeaders_octoCapabilities = string;
---
+++ src/models/DeleteWebhookRequest_id.ts
@@ -0,0 +1,8 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * The webhook id
+ */
+export type DeleteWebhookRequest_id = string;
